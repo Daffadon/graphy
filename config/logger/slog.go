5d9d99c8d6823f43cba21434e25df6cdfd67075e
daffadon <daffaputranarendra9@gmail.com>
2025-09-09 11:21:51 +0700

    feat(logger): implement ColorTextHandler for colored log output

--- config/logger/slog.go
+++ config/logger/slog.go
@@ -1,13 +1,40 @@
 package logger
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 	"os"
+
+	"github.com/fatih/color"
 )
 
+type ColorTextHandler struct {
+	slog.Handler
+}
+
+func (h *ColorTextHandler) Handle(ctx context.Context, r slog.Record) error {
+	var levelColor *color.Color
+	switch r.Level {
+	case slog.LevelInfo:
+		levelColor = color.New(color.FgGreen)
+	case slog.LevelWarn:
+		levelColor = color.New(color.FgYellow)
+	case slog.LevelError:
+		levelColor = color.New(color.FgRed)
+	default:
+		levelColor = color.New(color.FgWhite)
+	}
+	levelStr := r.Level.String()
+	msg := fmt.Sprintf("%s %s", levelStr, r.Message)
+	levelColor.Fprintf(os.Stdout, "%s\n", msg)
+	return nil
+}
+
 func NewSlog() *slog.Logger {
-	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	baseHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
 	})
-	return slog.New(handler)
+	colorHandler := &ColorTextHandler{Handler: baseHandler}
+	return slog.New(colorHandler)
 }
